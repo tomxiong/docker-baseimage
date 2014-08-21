b1f1890b5f398f6949bb5573e722310ba6fe6ba3
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-08-21 16:07:29 -0400

    Update utilities.sh

--- utilities.sh
+++ utilities.sh
@@ -4,7 +4,8 @@ source /build/buildconfig
 set -x
 
 ## Often used tools.
-$minimal_apt_get_install curl less nano vim psmisc
+$minimal_apt_get_install curl less nano psmisc
 
 ## This tool runs a command as another user and sets $HOME.
 cp /build/bin/setuser /sbin/setuser
+chmod 750 /sbin/setuser
