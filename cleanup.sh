3fb12ef88a344390ed1c1c111ffe00a46d328665
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-07-27 05:27:18 -0400

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -8,4 +8,8 @@ rm -rf /build
 rm -rf /tmp/* /var/tmp/*
 rm -rf /var/lib/apt/lists/*
 rm -f /etc/dpkg/dpkg.cfg.d/02apt-speedup
-
+# removing more stuff
+rm -rf /usr/share/man /usr/share/groff /usr/share/info
+rm -rf /usr/share/lintian /usr/share/linda /var/cache/man
+(( find /usr/share/doc -depth -type f ! -name copyright|xargs rm || true )) && \
+(( find /usr/share/doc -empty|xargs rmdir || true ))
