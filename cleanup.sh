a822d5ce516ba2a132c63b1faabffda23632ae2c
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-08-12 22:54:24 -0400

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -8,7 +8,7 @@ rm -rf /build
 rm -rf /tmp/* /var/tmp/*
 rm -rf /var/lib/apt/lists/*
 rm -f /etc/dpkg/dpkg.cfg.d/02apt-speedup
-# removing more stuff
-rm -rf /usr/share/man /usr/share/groff /usr/share/info
+# removing more stuff    remove removing /usr/share/man because problem with container with JAVA
+rm -rf /usr/share/groff /usr/share/info
 rm -rf /usr/share/lintian /usr/share/linda /var/cache/man
 (( find /usr/share/doc -depth -type f ! -name copyright|xargs rm || true ) && ( find /usr/share/doc -empty|xargs rmdir || true ))
